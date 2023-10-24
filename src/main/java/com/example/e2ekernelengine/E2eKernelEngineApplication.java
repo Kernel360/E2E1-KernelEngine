097c0b0c789e0e7f95dd22a951ac270f50ee2f34
anso33 <hyeon131812@gmail.com>
2023-10-24 16:32:42 +0900

    :tada: feat: Start KernelEngine project

---
+++ src/main/java/com/example/e2ekernelengine/E2eKernelEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.e2ekernelengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2eKernelEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2eKernelEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/e2ekernelengine/E2eKernelEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e2ekernelengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2eKernelEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
