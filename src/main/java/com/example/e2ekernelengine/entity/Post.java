b901d298c1afd5f203dd2c4a87169ede3f8bc963
aacara <budgielover@naver.com>
2023-10-24 18:19:04 +0900

    Chore: Annotate Blog relation

--- src/main/java/com/example/e2ekernelengine/entity/Post.java
+++ src/main/java/com/example/e2ekernelengine/entity/Post.java
@@ -1,33 +1,36 @@
 package com.example.e2ekernelengine.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-
 @Entity
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Post {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "post_id")
-    private Long postId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "post_id")
+	private Long postId;
 
-    @ManyToOne
-    @JoinColumn(name = "blog_id")
-    private Blog blog;
+	// TODO 블로그 객체 연결 필요
+	// @ManyToOne
+	// @JoinColumn(name = "blog_id")
+	// private Blog blog;
 
-    @Column(name = "post_url")
-    private String postUrl;
+	@Column(name = "post_url")
+	private String postUrl;
 
-    @Column(name = "post_title")
-    private String postTitle;
+	@Column(name = "post_title")
+	private String postTitle;
 
-    @Column(name = "post_detail")
-    private String postDetail;
+	@Column(name = "post_detail")
+	private String postDetail;
 }
