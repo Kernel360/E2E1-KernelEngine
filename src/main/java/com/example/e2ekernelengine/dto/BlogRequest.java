18b40ea64405ce4042b44d660fd3aa4a5018c240
김찬규 <kjkj5868@gmail.com>
2023-10-25 14:22:23 +0900

    feat : add Blog Dto

---
+++ src/main/java/com/example/e2ekernelengine/dto/BlogRequest.java
@@ -0,0 +1,24 @@
+package com.example.e2ekernelengine.dto;
+
+import com.example.e2ekernelengine.entity.Blog;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BlogRequest {
+	private String rss;
+	private String url;
+	private String description;
+
+	public Blog toEntity() {
+		return Blog.builder()
+				.rss(rss)
+				.url(url)
+				.description(description)
+				.build();
+	}
+}
