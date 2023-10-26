3c79b23f59c13f80a264cd0aae6168888c26e893
aacara <budgielover@naver.com>
2023-10-26 20:15:45 +0900

    Fix: keyword @GetMapping
    - @GetMapping 방식에 따라 바꿈

--- src/main/java/com/example/e2ekernelengine/feed/controller/FeedController.java
+++ src/main/java/com/example/e2ekernelengine/feed/controller/FeedController.java
@@ -5,12 +5,11 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.example.e2ekernelengine.feed.dto.request.FeedSearchRequest;
-import com.example.e2ekernelengine.feed.dto.response.FeedSearchResponse;
+import com.example.e2ekernelengine.feed.dto.response.FeedSearchResponseDto;
 import com.example.e2ekernelengine.feed.service.FeedService;
 
 import lombok.RequiredArgsConstructor;
@@ -24,19 +23,15 @@ public class FeedController {
 
 	//-- Read(Search)--//
 	@ResponseBody
-	@GetMapping("/search")
-	public ResponseEntity<List<FeedSearchResponse>> searchFeedByKeyword(
-			@RequestBody FeedSearchRequest feedSearchRequest) {
+	@GetMapping("/search/{keyword}")
+	public ResponseEntity<List<FeedSearchResponseDto>> searchFeedByKeyword(
+			@PathVariable String keyword) {
 
-		// 키워드를 담은 feedSearchRequest를 통째로 보내서 Feed 데이터를 검색
-		// 이유: 최대한 많은 로직을 service에서 처리하기 위함
-		List<FeedSearchResponse> feedResponses = feedService.searchFeedsByKeyword(feedSearchRequest);
+		List<FeedSearchResponseDto> feedResponses = feedService.searchFeedsByKeyword(keyword);
 
 		if (feedResponses.isEmpty()) {
-			// 검색 결과가 없는 경우 404 Not Found 응답
 			return ResponseEntity.notFound().build();
 		} else {
-			// 검색 결과가 있는 경우 200 OK 응답과 JSON 데이터 반환
 			return ResponseEntity.ok(feedResponses);
 		}
 	}
--- src/main/java/com/example/e2ekernelengine/feed/service/FeedService.java
+++ src/main/java/com/example/e2ekernelengine/feed/service/FeedService.java
@@ -7,14 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.e2ekernelengine.blog.db.entity.Blog;
 import com.example.e2ekernelengine.blog.db.repository.BlogJpaRepository;
-import com.example.e2ekernelengine.crawler.FeedData;
 import com.example.e2ekernelengine.feed.db.entity.Feed;
 import com.example.e2ekernelengine.feed.db.repository.FeedRepository;
-import com.example.e2ekernelengine.feed.dto.request.FeedSearchRequest;
-import com.example.e2ekernelengine.feed.dto.response.FeedSearchResponse;
-import com.example.e2ekernelengine.global.exception.NotFoundException;
+import com.example.e2ekernelengine.feed.dto.response.FeedSearchResponseDto;
 
 @Service
 public class FeedService {
@@ -27,29 +23,25 @@ public class FeedService {
 		this.blogJpaRepository = blogJpaRepository;
 	}
 
-	public List<FeedSearchResponse> searchFeedsByKeyword(FeedSearchRequest request) {
-		String keyword = request.getKeyword();
+	@Transactional
+	public List<FeedSearchResponseDto> searchFeedsByKeyword(String keyword) {
 
-		// FeedRepository를 사용하여 데이터베이스에서 Feed 검색
 		List<Feed> searchFeedsByKeyword = feedRepository.searchFeedsByKeyword(keyword);
 
-		// BlogJpaRepository를 사용하여 데이터베이스에서 keyword(이 경우 작성자의 이름)을 이용하여 Blog id 검색
 		List<Long> searchBlogsByBlogWriterName = blogJpaRepository.findBlogIdsByBlogWriterName(keyword);
 
-		// searchFeedsByKeyword의 Feed(Entity) -> FeedResponseList(dto)
-		List<FeedSearchResponse> feedResponseList = searchFeedsByKeyword.stream()
-				.map(feed -> FeedSearchResponse
+		List<FeedSearchResponseDto> feedResponseList = searchFeedsByKeyword.stream()
+				.map(feed -> FeedSearchResponseDto
 						.create(feed.getFeedId(),
 								feed.getFeedUrl(),
 								feed.getFeedTitle(),
 								feed.getFeedContent()))
 				.collect(Collectors.toList());
 
-		// Blog id로 검색된 모든 Blog에 속하는 Feed(Entity) -> FeedResponseList(dto)
 		for (Long blogId : searchBlogsByBlogWriterName) {
 			List<Feed> blogFeeds = feedRepository.searchFeedsByBlog_BlogId(blogId);
-			List<FeedSearchResponse> blogFeedResponses = blogFeeds.stream()
-					.map(feed -> FeedSearchResponse
+			List<FeedSearchResponseDto> blogFeedResponses = blogFeeds.stream()
+					.map(feed -> FeedSearchResponseDto
 							.create(feed.getFeedId(),
 									feed.getFeedUrl(),
 									feed.getFeedTitle(),
@@ -61,21 +53,5 @@ public class FeedService {
 		return feedResponseList;
 	}
 
-	@Transactional
-	public void saveNewFeedsByCrawler(List<FeedData> feedDataList, Long blogId) {
-
-		Blog blog = blogJpaRepository.findById(blogId).orElseThrow(() -> new NotFoundException("해당 블로그가 존재하지 않습니다."));
-		for (FeedData feedData : feedDataList) {
-			Feed feed = Feed.builder()
-					.blog(blog)
-					.feedUrl(feedData.getLink())
-					.feedTitle(feedData.getTitle())
-					.feedDescription(feedData.getDescription())
-					.feedCreatedAt(feedData.getPubDate())
-					.feedContent(feedData.getContent())
-					.build();
-			feedRepository.save(feed);
-		}
-	}
 }
 
