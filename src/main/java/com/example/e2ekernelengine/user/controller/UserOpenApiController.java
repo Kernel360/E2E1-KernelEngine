af13fad067ae309d9e7b1aa588617911b6438697
aacara <budgielover@naver.com>
2023-10-31 14:36:36 +0900

    feat: login(before implementing security)
    - 시큐리티로 변환 전 작동하는 login 코드 캡처

--- src/main/java/com/example/e2ekernelengine/user/controller/UserOpenApiController.java
+++ src/main/java/com/example/e2ekernelengine/user/controller/UserOpenApiController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.e2ekernelengine.user.dto.request.UserLoginRequestDto;
 import com.example.e2ekernelengine.user.dto.request.UserRegisterRequestDto;
 import com.example.e2ekernelengine.user.dto.response.UserResponseDto;
 import com.example.e2ekernelengine.user.service.UserService;
@@ -18,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/open-api/user")
+@RequestMapping("api/v1/open-api/user")
 public class UserOpenApiController {
 
 	private final UserService userService;
@@ -30,6 +31,15 @@ public class UserOpenApiController {
 			@RequestBody Optional<UserRegisterRequestDto> userRegisterRequestDto
 	) {
 		UserResponseDto userResponseDto = userService.register(userRegisterRequestDto);
+		return ResponseEntity.ok(userResponseDto);
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<UserResponseDto> login(
+			@Valid
+			@RequestBody Optional<UserLoginRequestDto> userLoginRequestDto
+	) {
+		UserResponseDto userResponseDto = userService.login(userLoginRequestDto);
 		return ResponseEntity.ok(userResponseDto);
 	}
 
--- src/main/java/com/example/e2ekernelengine/user/service/UserService.java
+++ src/main/java/com/example/e2ekernelengine/user/service/UserService.java
@@ -6,12 +6,16 @@ import javax.validation.Valid;
 
 import org.springframework.stereotype.Service;
 
+import com.example.e2ekernelengine.global.exception.NotFoundException;
 import com.example.e2ekernelengine.user.converter.UserConverter;
 import com.example.e2ekernelengine.user.db.entity.User;
 import com.example.e2ekernelengine.user.db.repository.UserRepository;
+import com.example.e2ekernelengine.user.dto.request.UserLoginRequestDto;
 import com.example.e2ekernelengine.user.dto.request.UserRegisterRequestDto;
 import com.example.e2ekernelengine.user.dto.response.UserResponseDto;
+import com.example.e2ekernelengine.user.exception.LoginException;
 import com.example.e2ekernelengine.user.exception.RegisterException;
+import com.example.e2ekernelengine.user.util.UserStatusType;
 
 import lombok.RequiredArgsConstructor;
 
@@ -41,5 +45,32 @@ public class UserService {
 		return userResponseDto;
 	}
 
+	public UserResponseDto login(@Valid Optional<UserLoginRequestDto> userLoginRequestDto) {
+
+		userLoginRequestDto.orElseThrow(() -> new LoginException("UserLoginRequestDto is empty."));
+
+		String userEmail = userLoginRequestDto.get().getUserEmail();
+		String userPassword = userLoginRequestDto.get().getUserPassword();
+		User loginUser = getUserIfExists(userEmail, userPassword);
+		UserResponseDto userResponseDto = userConverter.toDto(loginUser);
+		return userResponseDto;
+		/*var userEntity = userService.login(request.getEmail(), request.getPassword());
+		// 사용자 없으면 throw
+		// TODO 토큰 생성 로직으로 변경하기
+		return userConverter.toDto(userEntity);*/
+	}
+
+	public User getUserIfExists(String userEmail, String userPassword) {
+		return userRepository.findFirstByUserEmailAndUserPasswordAndUserStatusTypeOrderByUserIdDesc(
+				userEmail,
+				userPassword,
+				UserStatusType.ACTIVE
+		).orElseThrow(() -> new NotFoundException("User not found."));
+	}
+
+	/*public User me(Long userId) {
+		return userRepository.findFirstByUserIdAndUserStatusTypeOrderByUserIdDesc(
+		)
+	}*/
 }
 
