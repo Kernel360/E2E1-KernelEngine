e1fd0de8f7dbdfecbcf3ea69f187b1d759081873
anso33 <hyeon131812@gmail.com>
2023-11-18 15:45:50 +0900

    feat: Add docker-compose file for es & kibana

--- src/main/java/com/example/e2ekernelengine/global/config/ElasticSearchConfig.java
+++ src/main/java/com/example/e2ekernelengine/global/config/ElasticSearchConfig.java
@@ -1,5 +1,7 @@
 package com.example.e2ekernelengine.domain.search.config;
 
+import com.example.e2ekernelengine.domain.search.repository.FeedSearchRepository;
+import com.example.e2ekernelengine.domain.search.repository.FeedSearchRepositoryImpl;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -7,7 +9,7 @@ import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@EnableElasticsearchRepositories
+@EnableElasticsearchRepositories(basePackageClasses = {FeedSearchRepository.class, FeedSearchRepositoryImpl.class})
 @Configuration
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
