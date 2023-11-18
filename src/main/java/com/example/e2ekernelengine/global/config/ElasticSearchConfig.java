345395fa085ce5e593a8d6d43e6d75d51114b909
anso33 <hyeon131812@gmail.com>
2023-11-18 15:48:13 +0900

    feat: Add EnableRepository Options

--- src/main/java/com/example/e2ekernelengine/global/config/ElasticSearchConfig.java
+++ src/main/java/com/example/e2ekernelengine/global/config/ElasticSearchConfig.java
@@ -1,7 +1,5 @@
-package com.example.e2ekernelengine.domain.search.config;
+package com.example.e2ekernelengine.global.config;
 
-import com.example.e2ekernelengine.domain.search.repository.FeedSearchRepository;
-import com.example.e2ekernelengine.domain.search.repository.FeedSearchRepositoryImpl;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -9,7 +7,8 @@ import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@EnableElasticsearchRepositories(basePackageClasses = {FeedSearchRepository.class, FeedSearchRepositoryImpl.class})
+//@EnableElasticsearchRepositories(basePackageClasses = {FeedSearchRepository.class, FeedSearchRepositoryImpl.class})
+@EnableElasticsearchRepositories(basePackages = {"com.example.e2ekernelengine.domain.search.repository"})
 @Configuration
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
