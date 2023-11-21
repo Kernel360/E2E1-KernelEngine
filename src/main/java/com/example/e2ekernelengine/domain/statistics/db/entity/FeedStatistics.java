109c5570ef8eed993a4446d21aa3036b83e336a2
anso33 <hyeon131812@gmail.com>
2023-11-21 11:21:44 +0900

    refactor: Move module from batch to api-module

--- src/main/java/com/example/e2ekernelengine/domain/statistics/db/entity/FeedStatistics.java
+++ src/main/java/com/example/e2ekernelengine/domain/statistics/db/entity/FeedStatistics.java
@@ -1,14 +1,11 @@
-package com.example.modulebatch.feed.db.entity;
+package com.example.e2ekernelengine.domain.statistics.db.entity;
 
+import com.example.e2ekernelengine.domain.feed.db.entity.Feed;
 import java.time.LocalDateTime;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import com.example.e2ekernelengine.domain.feed.db.entity.Feed;
-
 import lombok.Getter;
 import lombok.ToString;
 
@@ -16,17 +13,21 @@ import lombok.ToString;
 @Getter
 @ToString
 public class FeedStatistics {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long feedStatId;
+
+
 	private Long feedId;
+
 	private LocalDateTime statisticsAt;
 
 	private Integer visitCount;
 
 	public static FeedStatistics create(final Feed feed) {
 		FeedStatistics statistics = new FeedStatistics();
-		statistics.visitCount = feed.getVisitCount();
+		statistics.visitCount = feed.getFeedVisitCount();
 		statistics.feedId = feed.getFeedId();
 		statistics.statisticsAt = LocalDateTime.now().minusDays(1);
 		return statistics;
