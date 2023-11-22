8ca633a23bc9f22115dbfd15dc37ca2192de553c
aacara <budgielover@naver.com>
2023-11-22 15:01:53 +0900

    Refactor: 데이터만 던져주는 api 화면과 함께 return하도록 변경

--- src/main/java/com/example/e2ekernelengine/domain/search/controller/EsSearchController.java
+++ src/main/java/com/example/e2ekernelengine/domain/search/controller/EsSearchController.java
@@ -2,7 +2,9 @@ package com.example.e2ekernelengine.domain.search.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,12 +17,12 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("api/v1/search")
+@RequestMapping("api/v1/feeds")
 public class EsSearchController {
 
 	private final EsSearchService esSearchService;
 	
-	@GetMapping()
+	/*@GetMapping("/search/accurate")
 	public ResponseEntity<Page<EsFeedPageableResponse>> search(
 			@RequestParam(value = "q") String keyword,
 			Pageable pageable) {
@@ -29,6 +31,32 @@ public class EsSearchController {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(feedPage);
+	}*/
+
+	@GetMapping("/search/accurate")
+	public String searchFeedBy(
+			@RequestParam(value = "q") String keyword,
+			@PageableDefault(size = 5, sort = "feedAccurate", direction = Sort.Direction.DESC) Pageable pageable,
+			Model model) {
+		Page<EsFeedPageableResponse> feedPage = esSearchService.searchFeedsByKeyword(keyword, pageable);
+		prepareModel(model, feedPage, keyword, "accurate");
+		return "searchResults";
+	}
+
+	private void prepareModel(Model model,
+			Page<EsFeedPageableResponse> feedPage,
+			String keyword,
+			String selectedSortOption) {
+		int currentPage = feedPage.getNumber();
+		int totalPages = feedPage.getTotalPages();
+		int startPage = Math.max(0, currentPage - 2);
+		int endPage = Math.min(currentPage + 2, totalPages - 1);
+
+		model.addAttribute("feedPage", feedPage);
+		model.addAttribute("query", keyword);
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
+		model.addAttribute("selectedSortOption", selectedSortOption);
 	}
 
 }
